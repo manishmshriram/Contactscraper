f79de19ded8d464a568386030553569726cab017
manishmshriram <130274205+manishmshriram@users.noreply.github.com>
2025-04-20 13:19:12 +0530

    requirement.txt.py

--- requirement.txt.py
+++ requirement.txt.py
@@ -1,2 +1,6 @@
-
-print("Hello from your first .py file!")
+streamlit==1.18.0
+pandas==1.5.3
+beautifulsoup4==4.11.1
+requests==2.28.0
+googlesearch-python==1.1.0
+openpyxl==3.0.10  # If you use Excel files, for reading and writing
