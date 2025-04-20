2e80d4cdde8ab9335cc1a01aa18dc68e7bd849d3
manishmshriram <130274205+manishmshriram@users.noreply.github.com>
2025-04-20 13:33:14 +0530

    requirement.txt.py

--- requirement.txt.py
+++ requirement.txt.py
@@ -1,6 +1,6 @@
-streamlit==1.18.0
-pandas==1.5.3
-beautifulsoup4==4.11.1
-requests==2.28.0
-googlesearch-python==1.1.0
-openpyxl==3.0.10  # If you use Excel files, for reading and writing
+streamlit
+pandas
+beautifulsoup4
+requests
+googlesearch-python
+openpyxl
